de2a4b164aaf4ab0b5c62ee7bff22d896158346f
Gregory Bonilla <bonillaj776@gmail.com>
2022-05-20 14:59:41 -0500

    Sensor camara listo, probado en dispositivo externo.

--- ecuadorTuristico.Android/MainActivity.cs
+++ ecuadorTuristico.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Plugin.CurrentActivity;
 
 namespace ecuadorTuristico.Droid
 {
@@ -13,7 +14,7 @@ namespace ecuadorTuristico.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
@@ -21,8 +22,9 @@ namespace ecuadorTuristico.Droid
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            
         }
     }
 }
